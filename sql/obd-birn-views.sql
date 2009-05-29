2f70a2b35a603f5c4537291286be86c148617456
Chris J. Mungall <cjm@berkeleybop.org>
2009-05-29 23:13:59 +0000

    all BIRN

--- sql/obd-birn-views.sql
+++ sql/obd-birn-views.sql
@@ -1,3 +1,112 @@
+SELECT  enforce_link_metadata();
+
+-- BEGIN MATERIALIZE
+-- SELECT create_matview('instance_of_link');
+-- CREATE INDEX instance_of_link_idx_node ON instance_of_link(node_id);
+-- CREATE INDEX instance_of_link_idx_object ON instance_of_link(object_id);
+-- CREATE INDEX instance_of_link_idx_node_object ON instance_of_link(node_id,object_id);
+-- END MATERIALIZE
+
+CREATE OR REPLACE VIEW instance_link_class_pair AS
+ SELECT
+  io.node_id,
+  link.predicate_id,
+  link.object_id,
+  io.object_id AS node_class_id,
+  yio.object_id AS object_class_id
+ FROM
+  instance_of_link AS io
+  INNER JOIN link ON (io.node_id=link.no1de_id)
+  INNER JOIN instance_of_link AS yio ON (link.object_id=yio.node_id)
+ WHERE
+  link.is_metadata='f';
+
+
+-- BEGIN MATERIALIZE
+-- SELECT create_matview('instance_link_class_pair');
+-- CREATE INDEX instance_link_class_pair_ix1 ON instance_link_class_pair(node_id);
+-- CREATE INDEX instance_link_class_pair_ix2 ON instance_link_class_pair(predicate_id);
+-- CREATE INDEX instance_link_class_pair_ix3 ON instance_link_class_pair(object_id);
+-- CREATE INDEX instance_link_class_pair_ix6 ON instance_link_class_pair(node_class_id,object_class_id);
+-- CREATE INDEX instance_link_class_pair_ix7 ON instance_link_class_pair(node_class_id,predicate_id,object_class_id);
+-- END MATERIALIZE
+
+-- we exclude classes such as (size of (spinal_cord)) of (spinal_cord)
+CREATE OR REPLACE VIEW instance_link_class_pair_nr AS
+ SELECT *
+ FROM instance_link_class_pair AS il
+  WHERE
+   NOT EXISTS (
+    SELECT * FROM link AS rl WHERE il.node_class_id=rl.node_id AND il.object_class_id=rl.object_id
+   )
+   AND
+   NOT EXISTS (
+    SELECT * FROM link AS rl WHERE il.object_class_id=rl.node_id AND il.node_class_id=rl.object_id
+   );
+
+
+-- BEGIN MATERIALIZE
+-- SELECT create_matview('instance_link_class_pair_nr');
+-- CREATE INDEX instance_link_class_pair_nr_ix1 ON instance_link_class_pair_nr(node_id);
+-- CREATE INDEX instance_link_class_pair_nr_ix2 ON instance_link_class_pair_nr(predicate_id);
+-- CREATE INDEX instance_link_class_pair_nr_ix3 ON instance_link_class_pair_nr(object_id);
+-- CREATE INDEX instance_link_class_pair_nr_ix6 ON instance_link_class_pair_nr(node_class_id,object_class_id);
+-- CREATE INDEX instance_link_class_pair_nr_ix7 ON instance_link_class_pair_nr(node_class_id,predicate_id,object_class_id);
+-- END MATERIALIZE
+
+
+-- size ~1m
+
+-- <x1 r y1>,<x2 r y2>,<X,Y>
+-- such that X(x1),X(x2),Y(y1),Y(y2)
+-- Large join.
+-- It is recommended you constrain this with at least a predicate
+CREATE OR REPLACE VIEW instance_common_ancestor_tuple AS
+ SELECT
+  il1.node_id,
+  il1.object_id,
+  il2.node_id AS node2_id,
+  il2.object_id AS object2_id,
+  il1.node_class_id,
+  il1.predicate_id,
+  il1.object_class_id
+ FROM
+  instance_link_class_pair_nr AS il1
+  INNER JOIN instance_link_class_pair_nr AS il2 USING(node_class_id,predicate_id,object_class_id);
+
+
+CREATE OR REPLACE VIEW instance_LCA_tuple AS
+ SELECT
+  *
+ FROM
+  instance_common_ancestor_tuple AS ica
+ WHERE
+  NOT EXISTS (
+    SELECT *
+    FROM
+      instance_common_ancestor_tuple AS nrica,
+      link AS rl,
+      link AS rl2
+    WHERE ica.node_id = nrica.node_id AND   -- same instance
+      ica.object_id = nrica.object_id AND
+      nrica.node_class_id = rl.node_id AND    -- X redundant with X'
+      ica.node_class_id = rl.object_id AND -- Y redundant with Y'
+      nrica.object_class_id = rl2.node_id AND
+      ica.object_class_id = rl2.object_id AND
+      (ica.node_class_id != nrica.node_class_id OR ica.object_class_id != nrica.object_class_id)); -- not equal
+
+
+
+  
+
+-- equivalencies throw NR check
+-- delete from node as n where uid like '_:%' and not exists (select * from link where link.object_id=n.node_id);
+-- delete from node as n where uid like '_:%' and not exists (select * from asserted_link as link where link.object_id=n.node_id);
+
+-- after creation, below
+delete from node as n where uid like '%^%' and node_id in (select node_id from sameas);
+delete from node as n where uid like '_:%' and node_id in (select node_id from sameas);
+
 -- NEW
 
 CREATE OR REPLACE VIEW is_organism AS
@@ -9,10 +118,27 @@ CREATE OR REPLACE VIEW is_organism AS
  WHERE
   c.uid='birnlex_ubo:birnlex_2';
 
--- e.g. (human with PD) is_bearer_of (P)
-UPDATE link SET reiflink_node_id=get_node_id('BIRN:generic_annotation') WHERE predicate_id=get_node_id('birnlex_ubo:birnlex_17') AND is_inferred='f' AND node_id IN (SELECT node_id FROM asserted_is_a_link WHERE object_id=get_node_id('birnlex_tax:birnlex_516')) AND object_id NOT IN (SELECT node_id FROM is_a_link WHERE object_id=get_node_id('NIF_OBI:birnlex_11013'));
+CREATE OR REPLACE VIEW is_disease_class AS
+ SELECT DISTINCT node_id
+ FROM
+  is_a_link WHERE object_id=get_node_id('NIF_OBI:birnlex_11013');
 
-CREATE OR REPLACE VIEW is_bearer_of AS SELECT * FROM link WHERE predicate_id IN (SELECT node_id FROM node WHERE uid='birnlex_ubo:birnlex_17');
+CREATE OR REPLACE VIEW is_a_human AS
+ SELECT DISTINCT node_id
+ FROM
+   asserted_is_a_link WHERE object_id=get_node_id('birnlex_tax:birnlex_516');
+
+CREATE OR REPLACE VIEW is_bearer_of_relation_node AS 
+ SELECT *
+ FROM node WHERE uid='birnlex_ubo:birnlex_17';
+
+CREATE OR REPLACE VIEW is_bearer_of AS SELECT * FROM link WHERE predicate_id IN (SELECT node_id FROM is_bearer_of_relation_node);
+
+
+-- e.g. (human with PD) is_bearer_of (P)
+UPDATE link SET reiflink_node_id=get_node_id('BIRN:generic_annotation') 
+ WHERE predicate_id IN (SELECT node_id FROM is_bearer_of_relation_node) AND is_inferred='f' AND node_id IN (SELECT node_id FROM is_a_human) AND object_id NOT IN (SELECT node_id FROM is_disease_class);
+
 SELECT realize_relation('OBO_REL:has_part');
 
 CREATE OR REPLACE VIEW has_part_bearer_of AS
@@ -92,7 +218,6 @@ CREATE OR REPLACE VIEW nr_inheres_in_with_types_c AS
   ni.object_id,
   oi.object_id;
 
-
 SELECT realize_relation('OBO_REL:towards');
 
 CREATE OR REPLACE VIEW asserted_towards_with_types_c AS
@@ -125,15 +250,20 @@ CREATE OR REPLACE VIEW asserted_part_of_types_c AS
 
 
 -- conservative
+-- e.g. Dopamine that part_of neuron; axon terminal in neostriatum
 SELECT 
    store_genus_differentium(node_type_id,get_node_id('OBO_REL:part_of'),object_type_id,get_node_id('BIRN:generic_annotation'))
  FROM asserted_part_of_types_c;
 -- should reason after this..
 
+-- the following are less necessary now we are precoordinating these up-front
+
+-- does not get degenerate in (axon terminal that part_of Neostr)
 SELECT 
    store_genus_differentium(node_type_id,get_node_id('OBO_REL:inheres_in'),object_type_id,get_node_id('BIRN:generic_annotation'))
  FROM asserted_inheres_in_with_types_c;
 
+-- gets degenerate in (axon terminal that part_of Neostr)
 SELECT 
    store_genus_differentium(node_type_id,get_node_id('OBO_REL:inheres_in'),object_type_id,get_node_id('BIRN:generic_annotation'))
  FROM nr_inheres_in_with_types_c;
@@ -142,9 +272,11 @@ SELECT
    store_genus_differentium(node_type_id,get_node_id('OBO_REL:towards'),object_type_id,get_node_id('BIRN:generic_annotation'))
  FROM asserted_towards_with_types_c;
 
+-- todo: restrict. currently includes q^inh(x^p(y))^inhp(y)
 SELECT 
    store_genus_differentium(node_id,get_node_id('OBO_REL:inheres_in_part_of'),object_id,get_node_id('BIRN:generic_annotation'))
- FROM inheres_in_part_of;
+ FROM inheres_in_part_of AS ip
+ WHERE NOT EXISTS (SELECT * FROM obo_rel.inheres_in AS i WHERE i.node_id=ip.node_id AND i.object_id=ip.object_id);
 
 SELECT realize_relation('OBO_REL:inheres_in_part_of');
 
@@ -167,7 +299,7 @@ CREATE OR REPLACE VIEW exemplifies AS
 -- INSERT INTO LINK (reiflink_node_id,node_id,predicate_id,object_id) (SELECT get_node_id('BIRN:generic_annotation'), node_id, predicate_id, object_id FROM has_part_bearer_of);
 -- SELECT reify_links_by_predicate('OBO_REL:exemplifies','BIRN:generic_annotation');
 INSERT INTO LINK (reiflink_node_id,node_id,predicate_id,object_id) (SELECT get_node_id('BIRN:generic_annotation'), node_id, predicate_id, object_id FROM exemplifies);
-
+-- DELETE FROM link WHERE predicate_id=get_node_id('OBO_REL:exemplifies');
 
 CREATE OR REPLACE VIEW instance_class_sim AS
  SELECT 
