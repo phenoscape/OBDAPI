1425b2f8eaec9a4cb7a4a77fb2e2499765d8845b
Cartik R. Kothari <cartik73@yahoo.com>
2009-06-24 01:33:48 +0000

    Added DDL for new phenotype metadata table with comments Modified DDL for phenotype EC table to add a new column for COUNT

--- sql/phenoscape/obd-phenoscape-schema.sql
+++ sql/phenoscape/obd-phenoscape-schema.sql
@@ -11,7 +11,8 @@ quality_uid VARCHAR,
 quality_label VARCHAR, 
 character_nid INTEGER, 
 character_uid VARCHAR, 
-character_label VARCHAR, 
+character_label VARCHAR,
+count VARCHAR, 
 entity_nid INTEGER, 
 entity_uid VARCHAR, 
 entity_label VARCHAR, 
@@ -46,6 +47,8 @@ COMMENT ON COLUMN phenotype_by_entity_character.quality_uid IS
 'The actual uid of the QUALITY. Eg: PATO:0000467';
 COMMENT ON COLUMN phenotype_by_entity_character.quality_label IS 
 'The actual label or name of the QUALITY. Eg: "present"';
+COMMENT ON COLUMN phenotype_by_entity_character.count IS 
+'The count of the ENTITY that is associated with the PHENOTYPE';
 COMMENT ON COLUMN phenotype_by_entity_character.character_nid IS 
 'The node id of the CHARACTER as stored in the NODE table';
 COMMENT ON COLUMN phenotype_by_entity_character.character_uid IS 
@@ -72,9 +75,101 @@ the link to the publication, character number, character text, state text, comme
 
 CREATE INDEX entity_character_index ON phenotype_by_entity_character(entity_nid, character_nid);
 CREATE INDEX subject_index ON phenotype_by_entity_character(subject_nid);
+CREATE INDEX entity_index ON phenotype_by_entity_character(entity_nid);
 
 COMMENT ON INDEX entity_character_index IS 
 'An index for ENTITY - CHARACTER combinations in the phenotype_with_entity_character table';
 
 COMMENT ON INDEX subject_index IS 
 'An index for SUBJECT column in the phenotype_with_entity_character table'; 
+
+COMMENT ON INDEX entity_index IS 
+'An index for ENTITY column in the phenotype_with_entity_character table'; 
+
+CREATE TABLE taxon_phenotype_metadata (
+row_number SERIAL PRIMARY KEY,
+reif_id INTEGER, 
+phenotype_uid VARCHAR, 
+taxon_uid VARCHAR, 
+taxon_label VARCHAR, 
+entity_uid VARCHAR, 
+entity_label VARCHAR, 
+quality_uid VARCHAR, 
+quality_label VARCHAR, 
+publication VARCHAR, 
+character_text VARCHAR, 
+character_comment VARCHAR, 
+character_number VARCHAR, 
+state_text VARCHAR, 
+state_comment VARCHAR, 
+curators VARCHAR
+);
+
+COMMENT ON TABLE taxon_phenotype_metadata IS 
+'A table that stores the metadata associated with every <TAXON><PHENOTYPE> assertion. 
+These include the name of the publication, the names of the curators, the character 
+and state text from the publication that has been curated, the character number from
+the matrix, and the free textcomments from the curators about the character and the state.
+In addition, we also store the taxon, entity and quality associated with the phenotype.';
+
+COMMENT ON COLUMN taxon_phenotype_metadata.reif_id IS 
+'The node id of the SUBJECT -> PHENOTYPE assertion as stored in the LINK table. This is 
+the link to the publication, character number, character text, state text, comments etc 
+(METADATA) that is associated with every SUBJECT -> PHENOTYPE assertion in the database';
+
+COMMENT ON COLUMN taxon_phenotype_metadata.phenotype_uid IS 
+'The actual uid of the PHENOTYPE [Eg. "PATO:0000052^OBO_REL:inheres_in(TAO:0001274)"]';
+
+COMMENT ON COLUMN taxon_phenotype_metadata.taxon_uid IS 
+'The actual uid of the TAXON. Eg. "TTO:1005925"';
+
+COMMENT ON COLUMN taxon_phenotype_metadata.taxon_label IS 
+'The actual label (name) of the TAXON. Eg. "Parapimelodus valenciennis"';
+
+COMMENT ON COLUMN taxon_phenotype_metadata.entity_uid IS 
+'The actual uid of the ENTITY. Eg. "TAO:0001274"';
+
+COMMENT ON COLUMN taxon_phenotype_metadata.entity_label IS 
+'The actual label (name) of the ENTITY. Eg. "coronomeckelian"';
+
+COMMENT ON COLUMN taxon_phenotype_metadata.quality_uid IS 
+'The actual uid of the QUALITY. Eg. "PATO:0000052"';
+
+COMMENT ON COLUMN taxon_phenotype_metadata.quality_label IS 
+'The actual label (name) of the QUALITY. Eg. "shape"';
+
+COMMENT ON COLUMN taxon_phenotype_metadata.publication IS 
+'The citation info of the publication the Taxon to Phenotype
+assertion is taken from. Eg. "de Pinna MCC. 1993. Higher-level 
+Phylogeny of Siluriformes (Teleostei: Ostariophysi), with a 
+New Classification of the Order. New York: City University of New York."';
+
+COMMENT ON COLUMN taxon_phenotype_metadata.character_text IS
+'The actual text from the publication about the character, which has been 
+curated as this Taxon to Phenotype assertion. Eg: "Coronoid bone in catfishes"';
+
+COMMENT ON COLUMN taxon_phenotype_metadata.character_comment IS
+'Comments from the curators on the curated character. Eg: "JGL thinks de Pinna 
+means coronomeckelian bone"';
+
+COMMENT ON COLUMN taxon_phenotype_metadata.character_number IS 
+'The number assigned to this character in the character-state
+data matrix. Eg: "42"';
+
+COMMENT ON COLUMN taxon_phenotype_metadata.state_text IS
+'The actual text from the publication about the state, which has been 
+curated as this Taxon to Phenotype assertion. Eg: "primitively an 
+inconspicuous bone on the lateral profile of the lower jaw, and its dorsal margin 
+does not reach the dorsal limit of the coronoid process"';
+
+COMMENT ON COLUMN taxon_phenotype_metadata.state_comment IS 
+'Comments from the curators on the curated state. Eg: "= coronomeckelian bone"';
+
+COMMENT ON COLUMN taxon_phenotype_metadata.curators IS 
+'The names of the curators who entered this Taxon - Phenotype assertion.
+Eg: "John G. Lundberg; Mark Sabaj Pérez"';
+
+CREATE INDEX reif_index ON taxon_phenotype_metadata(reif_id);
+
+COMMENT ON INDEX reif_index IS 
+'An index for REIF ID column in the taxon_phenotype_metadata table'; 
