0086f1de7b2e84b3ae90f47f4355b6aa2711b7ef
Jim Balhoff <balhoff@nescent.org>
2010-05-03 15:40:10 +0000

    Updates to Phenoscape views.

--- phenoscape/views/taxon.sql
+++ phenoscape/views/taxon.sql
@@ -9,10 +9,12 @@ SELECT
   parent_link.object_id AS parent_node_id
 FROM
   node taxon
+  -- this is sufficient only if there are only taxon nodes in the TTO
   JOIN node taxonomy ON (taxonomy.uid = 'teleost-taxonomy' AND taxon.source_id = taxonomy.node_id)
   JOIN node has_rank_rel ON (has_rank_rel.uid = 'has_rank')
   LEFT OUTER JOIN link has_rank_link ON (has_rank_link.node_id = taxon.node_id AND has_rank_link.predicate_id = has_rank_rel.node_id AND has_rank_link.is_inferred = FALSE)
   JOIN node is_extinct_rel ON (is_extinct_rel.uid = 'is_extinct')
   LEFT OUTER JOIN tagval ON (tagval.node_id = taxon.node_id AND tagval.tag_id = is_extinct_rel.node_id)
-  LEFT OUTER JOIN is_a_link parent_link ON (parent_link.node_id = taxon.node_id AND parent_link.is_inferred = FALSE)
+  JOIN node is_a_rel ON (is_a_rel.uid = 'OBO_REL:is_a')
+  LEFT OUTER JOIN link parent_link ON (parent_link.node_id = taxon.node_id AND parent_link.is_inferred = FALSE AND parent_link.predicate_id = is_a_rel.node_id)
 ;
--- phenoscape/views/term_relations.sql
+++ phenoscape/views/term_relations.sql
@@ -7,7 +7,7 @@ SELECT
   predicate.node_id AS predicate_id,
   predicate.uid AS predicate_uid,
   predicate.label AS predicate_label,
-  target.node_id AS object_id
+  target.node_id AS object_id,
   target.uid AS object_uid,
   target.label AS object_label,
   link.is_inferred AS is_inferrred
@@ -16,4 +16,6 @@ FROM
   JOIN node subject ON (subject.node_id = link.node_id)
   JOIN node predicate ON (predicate.node_id = link.predicate_id)
   JOIN node target ON (target.node_id = link.object_id)
-  ;
+WHERE 
+  link.combinator=''
+;
