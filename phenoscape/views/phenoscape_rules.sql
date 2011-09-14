1d53ce2ad4cf91a39c873bbdc5f604ec3ed7cdf7
Jim Balhoff <balhoff@nescent.org>
2011-09-14 18:45:12 +0000

    Rewrote rule for develops_from reasoning.

--- phenoscape/views/phenoscape_rules.sql
+++ phenoscape/views/phenoscape_rules.sql
@@ -1,13 +1,16 @@
 CREATE OR REPLACE VIEW absent_over_develops_from AS
 SELECT 
-    exhibits.node_id,
-    exhibits.predicate_id,
-    store_genus_differentium(is_absent.object_id, inheres_in.predicate_id, develops_from.node_id, NULL) AS object_id
+    phenotypeA.node_id,
+    (SELECT node_id FROM node WHERE uid = 'OBO_REL:is_a') AS predicate_id,
+    phenotypeB.node_id AS object_id
 FROM
-    link exhibits
-    JOIN link is_absent ON (is_absent.node_id = exhibits.object_id AND is_absent.predicate_id = (SELECT node_id FROM node WHERE uid = 'OBO_REL:is_a') AND is_absent.object_id = (SELECT node_id FROM node where uid = 'PATO:0000462'))
-    JOIN link inheres_in ON (inheres_in.node_id = exhibits.object_id AND inheres_in.predicate_id = (SELECT node_id FROM node WHERE uid = 'OBO_REL:inheres_in'))
-    JOIN link develops_from ON (develops_from.predicate_id = (SELECT node_id FROM node where uid = 'develops_from') AND develops_from.object_id = inheres_in.object_id)
-WHERE
-    exhibits.predicate_id = (SELECT node_id FROM node WHERE uid = 'PHENOSCAPE:exhibits')
+    node phenotypeA
+    JOIN link a_is_a ON (a_is_a.node_id = phenotypeA.node_id AND a_is_a.predicate_id = (SELECT node_id FROM node WHERE uid = 'OBO_REL:is_a') AND a_is_a.object_id = (SELECT node_id FROM node where uid = 'PATO:0000462') AND a_is_a.is_inferred = false)
+    JOIN link b_is_a ON (b_is_a.predicate_id = (SELECT node_id FROM node WHERE uid = 'OBO_REL:is_a') AND b_is_a.object_id = (SELECT node_id FROM node where uid = 'PATO:0000462') AND b_is_a.is_inferred = false)
+    JOIN node phenotypeB ON (phenotypeB.node_id = b_is_a.node_id)
+    JOIN link a_inheres_in ON (a_inheres_in.node_id = phenotypeA.node_id AND a_inheres_in.predicate_id = (SELECT node_id FROM node WHERE uid = 'OBO_REL:inheres_in') and a_inheres_in.is_inferred = false)
+    JOIN node entityA ON (entityA.node_id = a_inheres_in.object_id)
+    JOIN link b_inheres_in ON (b_inheres_in.node_id = phenotypeB.node_id AND b_inheres_in.predicate_id = (SELECT node_id FROM node WHERE uid = 'OBO_REL:inheres_in') and b_inheres_in.is_inferred = false)
+    JOIN node entityB ON (entityB.node_id = b_inheres_in.object_id)
+    JOIN link develops_from ON (develops_from.node_id = entityB.node_id AND develops_from.object_id = entityA.node_id AND develops_from.predicate_id = (SELECT node_id FROM node where uid = 'develops_from'))
 ;
